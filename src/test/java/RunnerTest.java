723099b76235b317420f8dfda45d1f6c3b42b5aa
jbueno <jbueno@vistajet.com>
2019-02-12 14:58:23 -0200

    adding report to Runner class

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features ={"src/test/resources/features/FirstFeature.feature"},
-        glue={"steps"}
+        glue={"steps"},
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
 )
 public class RunnerTest {
 
